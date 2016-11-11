e7a873b654417927a7f0240fc386f1f794b11123
jagmeetb <jagmeetbhamber@hotmail.com>
2016-11-11 14:39:33 -0500

    saves user's cities

--- app/src/main/java/jagmeet/weather/MainActivity.java
+++ app/src/main/java/jagmeet/weather/MainActivity.java
@@ -2,12 +2,15 @@ package jagmeet.weather;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -23,69 +26,20 @@ import java.util.List;
 import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
-
+	private SharedPreferences savedState;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		String result = LoadData("city.list.json");
-		List<String> finalResult = new ArrayList<String>();
-		List<Integer> idList = new ArrayList<Integer>();
-		try {
-			JSONObject jsonOBJ = new JSONObject(result);
-			JSONArray jsonARR = jsonOBJ.getJSONArray("list");
-
-			int jsonLEN = jsonARR.length();
-
-			for (int i = 0; i < jsonLEN; i++){
-				JSONObject jsonOBJ2 = jsonARR.getJSONObject(i);
-
-				String cityName = jsonOBJ2.getString("name");
-				String countryName = jsonOBJ2.getString("country");
-
-				int id = jsonOBJ2.getInt("_id");
-				finalResult.add(cityName + ", " + countryName);
-			}
-			String[] s = new String[finalResult.size()];
-			finalResult.toArray(s);
-
-			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-					this,
-					android.R.layout.simple_dropdown_item_1line,
-					finalResult
-			);
-
-			AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.cityInput);
-			textView.setThreshold(3);
-			textView.setAdapter(adapter);
-
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
+		savedState = getSharedPreferences("idSaved", MODE_PRIVATE);
+		String x = savedState.getString("list1", "");
+		TextView txt = (TextView) findViewById(R.id.message);
+	//	txt.setText(String.valueOf(x));
+		txt.setText(x);
 	}
 
-	public String LoadData(String inFile) {
-		String tContents = "";
 
-		try {
-			InputStream stream = getAssets().open(inFile);
-
-			int size = stream.available();
-			byte[] buffer = new byte[size];
-			stream.read(buffer);
-			stream.close();
-			tContents = new String(buffer);
-		} catch (IOException e) {
-			// Handle exceptions here
-		}
-
-		return tContents;
-	}
-
-	public void citySubmit(View view){
-		AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.cityInput);
-
-		Toast.makeText(getBaseContext(), "YOU SELECTED: " + textView.getText(), Toast.LENGTH_LONG).show();
+	public void cityAdd(View view){
+		startActivity(new Intent(this, SecondActivity.class));
 	}
 }
